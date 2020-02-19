771b3fb2b5e1bb53b6e5d4a46fe248da2783eb78
CodeMater125 <HenryRich14@gmail.com>
2020-02-19 18:19:19 -0500

    Added splash screen animation

--- app/src/main/java/com/mercury1089/scoutingapp2019/SplashActivity.java
+++ app/src/main/java/com/mercury1089/scoutingapp2019/SplashActivity.java
@@ -4,11 +4,14 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
@@ -16,8 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
 
-        TextView developersText = findViewById(R.id.CreditWhereCreditsDue);
-
+        /*
         ObjectAnimator anim = new ObjectAnimator().ofFloat(developersText, View.ALPHA, 1.0f, .75f);
         anim.setDuration(500);
         anim.setRepeatCount(ObjectAnimator.INFINITE);
@@ -26,24 +28,50 @@ public class SplashActivity extends AppCompatActivity {
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(anim);
         animatorSet.start();
+        */
 
-        Thread background = new Thread() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
             public void run() {
-                try {
-                    // Thread will sleep for 3 seconds
-                    sleep(3000);
+                ConstraintLayout constraintLayout = findViewById(R.id.SplashActivity);
+                ImageView lightningBolt = findViewById(R.id.LightningBolt);
+                TextView developersText = findViewById(R.id.CreditWhereCreditsDue);
 
-                    // After 3 seconds redirect to another intent
-                    Intent intent = new Intent(SplashActivity.this, PregameActivity.class);
-                    startActivity(intent);
+                int lightningBoltSpeed = 200;
 
-                    //Remove activity
-                    finish();
-                } catch (Exception e) {
-                }
+                ObjectAnimator animatorX = ObjectAnimator.ofFloat(lightningBolt, View.X, 100, 200).setDuration(lightningBoltSpeed);
+                ObjectAnimator animatorY = ObjectAnimator.ofFloat(lightningBolt, View.Y, -200, 300).setDuration(lightningBoltSpeed);
+                ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(lightningBolt, View.ALPHA, 0, 1).setDuration(lightningBoltSpeed);
+                ObjectAnimator animatorScreenAlphaOff = ObjectAnimator.ofFloat(constraintLayout, View.ALPHA, 1, 0).setDuration(100);
+                ObjectAnimator animatorTextAlpha = ObjectAnimator.ofFloat(developersText, View.ALPHA, 0, .5f).setDuration(0);
+                ObjectAnimator animatorScreenAlphaOn = ObjectAnimator.ofFloat(constraintLayout, View.ALPHA, 0, 1).setDuration(100);
+
+                ObjectAnimator anim = ObjectAnimator.ofFloat(developersText, View.ALPHA, .5f, 1.0f).setDuration(500);
+                anim.setRepeatCount(1);
+                anim.setRepeatMode(ObjectAnimator.REVERSE);
+
+                AnimatorSet lightningAnimation = new AnimatorSet();
+                lightningAnimation.playTogether(animatorX, animatorY, animatorAlpha);
+
+                AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playSequentially(lightningAnimation, animatorScreenAlphaOff, animatorTextAlpha, animatorScreenAlphaOn, anim);
+
+                animatorSet.start();
             }
-        };
-        // start thread
-        background.start();
+        }, 500);
+
+        Handler handler1 = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // After t seconds redirect to another intent
+                Intent intent = new Intent(SplashActivity.this, PregameActivity.class);
+                startActivity(intent);
+
+                //Remove activity
+                finish();
+            }
+        }, 2000);
     }
 }
